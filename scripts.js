af8eac611636a20ba041669fbca4b76c9e1be689
amaltr42 <amelia.travers@gmail.com>
2019-09-22 20:57:39 +1000

    added outputGridToHTML function

--- scripts.js
+++ scripts.js
@@ -29,8 +29,9 @@ function initialiseGrid(size) {
   for (var i=0; i < size; i++){
     //initialising the row//
     grid[i] = [];
+    //initialising the columns, one for each row//
     for (var j=0; j < size; j++){
-      //filling the grid with zeros//
+      //filling the grid with zeros to check if tile is already placed or not//
       grid[i][j] = 0;
     }
   }
@@ -42,7 +43,7 @@ function setMaxTilesFromInput(){
   tileInput = inputValue;
   //calculate map width and height, rounded squareroot of user input//
   mapWidthHeight = Math.round(Math.sqrt(tileInput));
-  //total tiles equals map with height squared//
+  //total tiles equals map height squared//
   maxTiles = mapWidthHeight * mapWidthHeight;
   //feed map width and height into grid initilisation//
   initialiseGrid(mapWidthHeight);
@@ -56,13 +57,24 @@ function updateResult(result){
   document.getElementById("result").innerHTML = output;
 }
 
-function placeTile(location, currentTile) {
-  console.log("current tile;", currentTile);
-  console.log("location in grid: ", location);
+//make the grid output accessable to HTML//
+function outputGridToHTML(){
+  var output = "";
+  for (var i=0; i < grid.length; i++){
+    //append current tile type to output//
+    for (var j=0; j < grid.length; j++){
+      output += grid[i][j].type;
+    }
+    output += "\n";
+  }
+  console.log("output grid: ", output);
+}
 
+function placeTile(location, currentTile) {
   //have I reached the max number of tiles? if so, STOP//
   if (placedTiles >= maxTiles) {
     console.log("final grid:", grid);
+    outputGridToHTML();
     return false;
   }
   //is the location empty? if not, STOP//
@@ -87,12 +99,14 @@ function placeTile(location, currentTile) {
     //only include if tile.type is an element in currentTile.neighbour//
     return result;
   });
+  //Are we moving to the next row?//
+  const shouldGoToNewRow = location.x + 1 >= mapWidthHeight;
   //choose a random tile based on allowed tile array//
-  const nextTile = allowedTiles[Math.floor(Math.random()*allowedTiles.length)];
+  const nextTile = shouldGoToNewRow ? tileTypes[Math.floor(Math.random()*tileTypes.length)] : allowedTiles[Math.floor(Math.random()*allowedTiles.length)];
   //repeat function with next tile and next location//
   const nextLocation = {
-    x: location.x + 1 >= mapWidthHeight ? 0 : location.x + 1,
-    y: location.x + 1 >= mapWidthHeight ? location.y + 1 : location.y
+    x: shouldGoToNewRow ? 0 : location.x + 1,
+    y: shouldGoToNewRow ? location.y + 1 : location.y
   }
   placeTile(nextLocation, nextTile);
 }
@@ -101,7 +115,6 @@ document.getElementById("calculateButton").onclick = function(){
   placedTiles = 0;
   //choose a random tile to start with//
   const originTile = tileTypes[Math.floor(Math.random()*tileTypes.length)];
-  console.log(originTile);
   const origin = {
     x: 0,
     y: 0
